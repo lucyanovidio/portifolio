2ef22652e2a00148895e023a487144cafb9e865b
Lucyan Ovídio <lucyanovidio@gmail.com>
2022-11-21 12:11:11 -0300

    Adicionando projeto Orange Evolution

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,111 +1,112 @@
-import { desafios } from './desafios.js'
-import { projetos } from './projetos.js'
+import { desafios } from "./desafios.js";
+import { projetos } from "./projetos.js";
 
-const navigation = document.querySelector('#navigation')
-const backToTopButton = document.querySelector('#backToTopButton')
-const toggle = document.querySelector('#sw-checkbox')
-const projectsSection = document.querySelector('#projects .wrapper')
+const navigation = document.querySelector("#navigation");
+const backToTopButton = document.querySelector("#backToTopButton");
+const toggle = document.querySelector("#sw-checkbox");
+const projectsSection = document.querySelector("#projects .wrapper");
 
-const notebook_1 = document.querySelector('#notebook-1')
-const notebook_2 = document.querySelector('#notebook-2')
-const notebook_2_white = document.querySelector('#notebook-2-white')
-const vidro = document.querySelector('#vidro')
+const notebook_1 = document.querySelector("#notebook-1");
+const notebook_2 = document.querySelector("#notebook-2");
+const notebook_2_white = document.querySelector("#notebook-2-white");
+const vidro = document.querySelector("#vidro");
 
+window.addEventListener("load", function begin() {
+  projetos(projectsSection);
+  const desafioBtn = document.querySelector("#desafio");
 
-window.addEventListener('load', function begin() {
-  projetos(projectsSection)
-  const desafioBtn = document.querySelector('#desafio')
-  
-  desafioBtn.addEventListener('click', () => {
-    desafios(projectsSection)
-    document.querySelector('#backToProjectsBtn').addEventListener('click', begin)
-  })
-})
+  desafioBtn.addEventListener("click", () => {
+    desafios(projectsSection);
+    document
+      .querySelector("#backToProjectsBtn")
+      .addEventListener("click", begin);
+  });
+});
 
-window.addEventListener('scroll', onScroll)
-onScroll()
+window.addEventListener("scroll", onScroll);
+onScroll();
 
 window.onload = setTimeout(() => {
-  notebook_1.style.opacity = 0
+  notebook_1.style.opacity = 0;
 
-  notebook_1.style.animation = 'none'
-  notebook_2.style.animation = 'none'
-  notebook_2_white.style.animation = 'none'
-  vidro.style.animation = 'none'
-}, 4000)
+  notebook_1.style.animation = "none";
+  notebook_2.style.animation = "none";
+  notebook_2_white.style.animation = "none";
+  vidro.style.animation = "none";
+}, 4000);
 
 function onScroll() {
-  showNavOnScroll()
-  showBackToTopButtonOnScroll()
+  showNavOnScroll();
+  showBackToTopButtonOnScroll();
 
-  activateMenuAtCurrentSection(about)
-  activateMenuAtCurrentSection(projects)
-  activateMenuAtCurrentSection(knowledge)
-  activateMenuAtCurrentSection(contact)
+  activateMenuAtCurrentSection(about);
+  activateMenuAtCurrentSection(projects);
+  activateMenuAtCurrentSection(knowledge);
+  activateMenuAtCurrentSection(contact);
 }
 
 function activateMenuAtCurrentSection(section) {
-  const targetLine = scrollY + innerHeight / 2
-  const sectionTop = section.offsetTop
-  const sectionHeight = section.offsetHeight
+  const targetLine = scrollY + innerHeight / 2;
+  const sectionTop = section.offsetTop;
+  const sectionHeight = section.offsetHeight;
 
-  const sectionTopReachOrPassedTargetLine = targetLine >= sectionTop
-  const sectionEndsAt = sectionTop + sectionHeight
-  const sectionEndPassedTargetLine = sectionEndsAt <= targetLine
+  const sectionTopReachOrPassedTargetLine = targetLine >= sectionTop;
+  const sectionEndsAt = sectionTop + sectionHeight;
+  const sectionEndPassedTargetLine = sectionEndsAt <= targetLine;
 
   const sectionBoundaries =
-    sectionTopReachOrPassedTargetLine && !sectionEndPassedTargetLine
+    sectionTopReachOrPassedTargetLine && !sectionEndPassedTargetLine;
 
-  const sectionId = section.getAttribute('id')
-  const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`)
+  const sectionId = section.getAttribute("id");
+  const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`);
 
-  menuElement.classList.remove('active')
+  menuElement.classList.remove("active");
 
   if (sectionBoundaries) {
-    menuElement.classList.add('active')
+    menuElement.classList.add("active");
   }
 }
 
 function showNavOnScroll() {
   if (scrollY > 0) {
-    navigation.classList.add('scroll')
+    navigation.classList.add("scroll");
   } else {
-    navigation.classList.remove('scroll')
+    navigation.classList.remove("scroll");
   }
 }
 
 function showBackToTopButtonOnScroll() {
   if (scrollY > 550) {
-    backToTopButton.classList.add('show')
+    backToTopButton.classList.add("show");
   } else {
-    backToTopButton.classList.remove('show')
+    backToTopButton.classList.remove("show");
   }
 }
 
-openMenu()
+openMenu();
 function openMenu() {
-  const openBtns = document.querySelectorAll('.open')
+  const openBtns = document.querySelectorAll(".open");
   openBtns.forEach((e) => {
-    e.addEventListener('click', () => {
-      document.body.classList.add('menu-expanded')
-    })
-  })
+    e.addEventListener("click", () => {
+      document.body.classList.add("menu-expanded");
+    });
+  });
 }
 
-closeMenu()
+closeMenu();
 function closeMenu() {
-  const closeBtns = document.querySelectorAll('.close')
+  const closeBtns = document.querySelectorAll(".close");
   closeBtns.forEach((e) => {
-    e.addEventListener('click', () => {
-      document.body.classList.remove('menu-expanded')
-    })
-  })
+    e.addEventListener("click", () => {
+      document.body.classList.remove("menu-expanded");
+    });
+  });
 }
 
 ScrollReveal({
-  origin: 'bottom',
-  distance: '50px',
-  duration: 1000
+  origin: "bottom",
+  distance: "50px",
+  duration: 1000,
 }).reveal(
   `#home, 
   #home img, 
@@ -121,8 +122,8 @@ ScrollReveal({
   #knowledg .card,
   #contact,
   #contact header`
-)
+);
 
-toggle.addEventListener('change', () => {
-  document.body.classList.toggle('light-mode')
-})
+toggle.addEventListener("change", () => {
+  document.body.classList.toggle("light-mode");
+});
--- assets/js/projetos.js
+++ assets/js/projetos.js
@@ -1,10 +1,51 @@
 function projetos(section) {
-  section.innerHTML =
-    `<header>
+  section.innerHTML = `<header>
       <h2>Projetos</h2>
     </header>
     <div class="content">
       <div class="cards">
+        <div class="card">
+          <h3>Orange Evolution</h3>
+          <div class="img-wrapper">
+            <img
+              src="./assets/images/projects/orange-evolution.gif"
+              alt="GIF do projeto Orange Evolution."
+            />
+            <div class="buttons">
+              <a href="https://github.com/squad-30/OrangeEvolution-Squad30" target="_blank" title="Repositório no Github">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  width="24"
+                  height="24"
+                >
+                  <path fill="none" d="M0 0h24v24H0z" />
+                  <path
+                    d="M12 2C6.475 2 2 6.475 2 12a9.994 9.994 0 0 0 6.838 9.488c.5.087.687-.213.687-.476 0-.237-.013-1.024-.013-1.862-2.512.463-3.162-.612-3.362-1.175-.113-.288-.6-1.175-1.025-1.413-.35-.187-.85-.65-.013-.662.788-.013 1.35.725 1.538 1.025.9 1.512 2.338 1.087 2.912.825.088-.65.35-1.087.638-1.337-2.225-.25-4.55-1.113-4.55-4.938 0-1.088.387-1.987 1.025-2.688-.1-.25-.45-1.275.1-2.65 0 0 .837-.262 2.75 1.026a9.28 9.28 0 0 1 2.5-.338c.85 0 1.7.112 2.5.337 1.912-1.3 2.75-1.024 2.75-1.024.55 1.375.2 2.4.1 2.65.637.7 1.025 1.587 1.025 2.687 0 3.838-2.337 4.688-4.562 4.938.362.312.675.912.675 1.85 0 1.337-.013 2.412-.013 2.75 0 .262.188.574.688.474A10.016 10.016 0 0 0 22 12c0-5.525-4.475-10-10-10z"
+                    fill="rgba(255,255,255,1)"
+                  />
+                </svg>
+              </a>
+              <a href="https://orangeevolution-squad30.up.railway.app" target="_blank" title="Ver Projetos">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  width="24"
+                  height="24"
+                >
+                  <path fill="none" d="M0 0h24v24H0z" />
+                  <path
+                    d="M12 3c5.392 0 9.878 3.88 10.819 9-.94 5.12-5.427 9-10.819 9-5.392 0-9.878-3.88-10.819-9C2.121 6.88 6.608 3 12 3zm0 16a9.005 9.005 0 0 0 8.777-7 9.005 9.005 0 0 0-17.554 0A9.005 9.005 0 0 0 12 19zm0-2.5a4.5 4.5 0 1 1 0-9 4.5 4.5 0 0 1 0 9zm0-2a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5z"
+                  />
+                </svg>
+              </a>
+            </div>
+          </div>
+          <p>
+            Plataforma de estudos desenvolvida na Hackathon do Programa de Fromação 4 da FCamara.
+          </p>
+          <p>HTML - CSS - JS - NodeJS - Express - SQLite</p>
+        </div>
         <div class="card">
           <h3>Desafio 21 dias de código</h3>
           <div class="img-wrapper">
@@ -45,7 +86,7 @@ function projetos(section) {
           <p>
             Desafio promovido pela Rocketseat com 21 projetos diferentes.
           </p>
-          <p>HTML - CSS- JS</p>
+          <p>HTML - CSS - JS</p>
         </div>
         <div class="card">
           <h3>Nutricionista Roberto - Links</h3>
@@ -82,7 +123,7 @@ function projetos(section) {
             </div>
           </div>
           <p>Páginas de links desenvolvida para cliente.</p>
-          <p>HTML - CSS- JS</p>
+          <p>HTML - CSS - JS</p>
         </div>
         <div class="card">
           <h3>Nutricionista Roberto - Página de consulta</h3>
@@ -119,7 +160,7 @@ function projetos(section) {
             </div>
           </div>
           <p>Página de contato para consultas desenvolvida para cliente.</p>
-          <p>HTML - CSS- JS</p>
+          <p>HTML - CSS - JS</p>
         </div>
         <div class="card">
           <h3>Doctor Care</h3>
@@ -156,7 +197,7 @@ function projetos(section) {
             </div>
           </div>
           <p>Projeto de site desenvolvido na Next Level Week Return da Rocketseat.</p>
-          <p>HTML - CSS- JS</p>
+          <p>HTML - CSS - JS</p>
         </div>
         <div class="card">
           <h3>Meu Portifólio</h3>
@@ -196,7 +237,7 @@ function projetos(section) {
             O presente site que desenvolvi para apresentar a mim e meus
             projetos.
           </p>
-          <p>HTML - CSS- JS</p>
+          <p>HTML - CSS - JS</p>
         </div>
         <div class="card">
           <h3>Em breve...</h3>
@@ -230,7 +271,7 @@ function projetos(section) {
           </svg>
         </div>
       </div>
-    </div>`
+    </div>`;
 }
 
-export { projetos }
+export { projetos };
